8f9142d3e9ec405dbbd7c6ae4031289fed76fca8
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2020-11-05 15:28:27 +0100

    Bump version to 1.0 (#3)
    MNT: Re-rendered with conda-build 3.20.5, conda-smithy 3.8.5, and conda-forge-pinning 2020.11.04.10.36.34

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,16 +2,17 @@
 set -ex
 
 cmake_options=(
-   "-GNinja"
-   "-DCMAKE_BUILD_TYPE=Release"
    "-DCMAKE_INSTALL_PREFIX=${PREFIX}"
+   "-DCMAKE_INSTALL_LIBDIR=lib"
    "-DBUILD_SHARED_LIBS=ON"
+   "-GNinja"
    ".."
 )
 
 mkdir -p _build
 pushd _build
-
 cmake "${cmake_options[@]}"
 
 ninja all install
+
+popd
