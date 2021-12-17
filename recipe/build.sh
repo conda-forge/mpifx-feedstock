3f535b9a1ac74d4f63d6c5c5a863165fdea2fe02
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2021-12-17 10:41:16 +0100

    Set OpenMPI variables in build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -ex
 
+if [ "${mpi}" == "openmpi" ]; then
+   export OPAL_PREFIX=$PREFIX
+   export OMPI_MCA_plm=isolated
+   export OMPI_MCA_btl_vader_single_copy_mechanism=none
+   export OMPI_MCA_rmaps_base_oversubscribe=yes
+fi
+
 cmake_options=(
    "-DCMAKE_INSTALL_PREFIX=${PREFIX}"
    "-DCMAKE_INSTALL_LIBDIR=lib"
